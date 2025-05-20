d6b52bbf3fce74c50b7ba8593a1d0764fdc20130
beanSsong <50390246+beanSsong@users.noreply.github.com>
2025-05-20 21:39:36 +0900

    Update 08. 결측치 처리 및 중복 데이터 제거.m
    unique(data, ... ) to unique(data_nan, ...)

--- 08. 결측치 처리 및 중복 데이터 제거.m
+++ 08. 결측치 처리 및 중복 데이터 제거.m
@@ -3,4 +3,4 @@ data = readtable(‘00.toy_example_full.csv');
 % Remove Column with NaN values
 data_nan = rmmissing(data, 2);
 
-data_uniq = unique(data, 'rows', 'stable')
+data_uniq = unique(data_nan, 'rows', 'stable')
